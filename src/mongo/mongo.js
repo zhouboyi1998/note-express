93fb1f2b13d5d99dc5d3162c23899de94523ae73
zhouboyi <1144188685@qq.com>
2022-06-23 00:42:10 +0800

    feat(command): 添加Body-Parser中间件依赖，实现增删改查功能
    添加获取所有 Linux 命令名称的接口
    
    引入Express中间件Body-Parser解析请求体参数，实现增、删、改功能
    
    修改README

--- src/mongo/mongo.js
+++ src/mongo/mongo.js
@@ -1,32 +1,38 @@
 // 引入 Mongoose 包, 创建 MongoDB 客户端
-const Mongoose = require('mongoose')
-const uri = "mongodb://note:123456@127.0.0.1:27017/note"
+const Mongoose = require("mongoose")
+const URI = "mongodb://note:123456@127.0.0.1:27017/note"
 
 // 连接 MongoDB 服务端
-Mongoose.connect(uri, (err) => {
+Mongoose.connect(URI, (err) => {
     if (err) {
         console.log(err)
         return
     } else {
-        console.log("Success to connect MongoDB")
+        console.log("Connect success --> " + URI)
     }
 })
 
-// Linux 命令模型
-const Command = new Mongoose.Schema({
-    id: Mongoose.ObjectId,
-    command: String,
-    usage: String,
-    params: [
-        {
-            param: String,
-            description: String
-        }
-    ]
-})
+// Linux 命令模型架构
+const CommandSchema = new Mongoose.Schema(
+    {
+        _id: Mongoose.ObjectId,
+        command: String,
+        usage: String,
+        params: [
+            {
+                param: String,
+                description: String
+            }
+        ]
+    },
+    {
+        // 不需要插入版本号
+        versionKey: false
+    }
+)
 
-// 使用模型获取连接
-const Connection = Mongoose.model("Command", Command, "linux_command")
+// 连接文档集合, 获取 Linux 命令模型
+const CommandModel = Mongoose.model("Command", CommandSchema, "linux_command")
 
-// 将连接导出给其它文件使用
-module.exports = { Connection }
+// 将模型导出
+module.exports = { CommandModel }
